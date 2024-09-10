c3eabacf0f426237756b0d855d490d0c6262ccf8
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-09-11 02:45:32 +0300

    Commit for changes

---
+++ src/joi/user.mjs
@@ -0,0 +1,16 @@
+import Joi from 'joi';
+
+const userSchema = Joi.object({
+  id: Joi.number().integer().positive().optional(), // Optional because it will be auto-incremented
+  username: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(8).required(),
+  passwordConfirmation: Joi.string().valid(Joi.ref('password')).required(),
+  role: Joi.string().valid('freelancer', 'client', 'admin').default('client'),
+  profilePictureUrl: Joi.string().uri().optional(),
+  bio: Joi.string().optional(),
+  createdAt: Joi.date().optional(), // Optional because it will be set by default
+  updatedAt: Joi.date().optional(), // Optional because it will be set automatically
+});
+
+export default userSchema;
