ce75ef3437f281d2413508e9588ba883a89b2def
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-09-11 04:23:06 +0300

    COmmit for changes

--- src/db/prisma.mjs
+++ src/db/prisma.mjs
@@ -2,4 +2,8 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+// Here define before and after hooks
+
+async function signUp(userData) {}
+
 export default prisma;
