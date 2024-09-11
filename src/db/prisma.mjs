0932127545e8d1e4a1f0979f8bb62879f9471f4d
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-09-11 03:24:34 +0300

    Commit for changes

--- src/db/prisma.mjs
+++ src/db/prisma.mjs
@@ -1,5 +1,13 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient().$extends({
+  model: {
+    user: {
+      async signUp(userData) {
+        // const hashedPassword = await
+      },
+    },
+  },
+});
 
 export default prisma;
