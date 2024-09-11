4abdf2b9a75a3df4118be68006dcfc92d7427f22
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-09-11 04:59:37 +0300

    Commit for changes

--- src/controllers/userController.mjs
+++ src/controllers/userController.mjs
@@ -18,20 +18,26 @@ const getAllUsers = async (req, res, next) => {
 // Define the createUser controller function
 const createUser = async (req, res, next) => {
   try {
+    // Get raw data from request.body
     const rawData = req.body;
+    // Validate data with Joi user schema this will return obj with error if data is not valid
     const validData = await userSchema.validateAsync(rawData);
+    // If Data is not valid return next(error)
     if (validData.error) {
       return next(new Error(validData.error));
     }
+    // Remove password confirm field from valid data because our Prisma model do not have this field
     delete validData.passwordConfirmation;
+    // Hash password
     const hashedPassword = await bcrypt.hash(validData.password, 10);
-
+    // Create new user with hashed password
     const freshUser = await prisma.user.create({
       data: {
         ...validData,
         password: hashedPassword,
       },
     });
+    // Return response
     res.status(200).json({
       status: `success`,
       user: freshUser,
