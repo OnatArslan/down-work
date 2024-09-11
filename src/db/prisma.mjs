a53d3f99e867de07a433763da17b96e2fbf3ab61
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-09-11 04:21:05 +0300

    Commit for changes

--- src/db/prisma.mjs
+++ src/db/prisma.mjs
@@ -2,20 +2,5 @@ import { PrismaClient } from '@prisma/client';
 import bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
-// .$extends({
-//   model: {
-//     user: {
-//       async signUp(userData) {
-//         const hashedPassword = await bcrypt.hash(userData.password, 10);
-//         return await prisma.user.create({
-//           data: {
-//             ...userData,
-//             password: hashedPassword,
-//           },
-//         });
-//       },
-//     },
-//   },
-// });
 
 export default prisma;
