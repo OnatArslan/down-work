90c8c2ce9d36e44b1a06d77f54ee020cc4207dcd
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-09-15 08:08:20 +0300

    Commit for changes

--- src/routers/authRouter.mjs
+++ src/routers/authRouter.mjs
@@ -1,9 +1,16 @@
 import express from 'express';
-import { signIn, signUp, logOut } from '../controllers/authController.mjs';
+import {
+  signIn,
+  signUp,
+  logOut,
+  getMe,
+  verify,
+} from '../controllers/authController.mjs';
 
 const router = express.Router();
 
 router.route(`/sign-up`).post(signUp);
 router.route(`/sign-in`).post(signIn);
 router.route(`/log-out`).delete(logOut);
+router.route(`/get-me`).get(verify, getMe);
 export default router;
