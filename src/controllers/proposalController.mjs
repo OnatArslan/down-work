dcf6574604916327665ba7c1c6bcb1a333a52417
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-09-15 07:56:59 +0300

    Commit for changes

--- src/controllers/proposalController.mjs
+++ src/controllers/proposalController.mjs
@@ -3,6 +3,7 @@ import prisma from '../db/prisma.mjs';
 export const sendProposal = async (req, res, next) => {
   try {
     const { text, price } = req.body;
+    // Try to get job with given ID and make sure job is open
     const job = await prisma.job.findUnique({
       where: {
         id: Number(req.params.jobId),
@@ -17,13 +18,15 @@ export const sendProposal = async (req, res, next) => {
         },
       },
     });
+    // If job is not find return error
     if (!job) {
       return next(new Error(`Can not any open job for your proposal`));
     }
+    // If job is current users post return error
     if (job.employerId === req.user.id) {
       return next(new Error(`Can not propose your own job post`));
     }
-
+    // Create proposal with given body data
     const newProposal = await prisma.proposal.create({
       data: {
         ...req.body,
@@ -31,14 +34,25 @@ export const sendProposal = async (req, res, next) => {
         jobId: Number(req.params.jobId),
       },
     });
+    // If some reason proposal not created return error
     if (!newProposal) {
       return next(new Error(`Can not create proposal`));
     }
-
+    // Send notification to job employer
+    const notification = await prisma.notification.create({
+      data: {
+        subject: `New proposal for ${job.title} job post`,
+        text: `Hi there,freelancer ${req.user.username} proposed to your ${job.title} job post.Message him to get contact`,
+        receiverId: job.employerId,
+      },
+    });
+    if (!notification) {
+      return next(new Error(`Can not send proposal`));
+    }
     res.status(200).json({
       status: `success`,
+      message: `Hey your proposal is send to user ${job.employer.username} successfully.`,
       data: {
-        job,
         newProposal,
       },
     });
