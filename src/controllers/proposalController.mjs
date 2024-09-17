b89a0e82ed90125b11eb13948f980adaf273a26a
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-09-17 12:17:48 +0300

    Commit for changes

--- src/controllers/proposalController.mjs
+++ src/controllers/proposalController.mjs
@@ -216,6 +216,9 @@ export const getProposals = async (req, res, next) => {
 // Answer proposal then send Contract
 export const answerProposal = async (req, res, next) => {
   try {
+    res.status(200).json({
+      status: `success`,
+    });
   } catch (error) {
     next(error);
   }
