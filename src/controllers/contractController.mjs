582c2f74eaeac958836c5aa90f20852a8add340d
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-09-19 17:49:05 +0300

    Commit for contract controller

--- src/controllers/contractController.mjs
+++ src/controllers/contractController.mjs
@@ -1,7 +1,32 @@
+import prisma from '../db/prisma.mjs';
+
 export const getAllContracts = async (req, res, next) => {
   try {
+    let contracts;
+    if (req.user.role === `client`) {
+      contracts = await prisma.contract.findMany({
+        where: {
+          clientId: Number(req.user.id),
+        },
+        include: {
+          freelancer: {
+            select: {
+              username: true,
+              email: true,
+            },
+          },
+        },
+      });
+    } else if (req.user.role === `freelancer`) {
+      contracts = await prisma.contract.findMany({
+        where: {
+          freelancerId: Number(req.user.id),
+        },
+      });
+    }
     res.status(200).json({
       status: `success`,
+      contracts,
     });
   } catch (error) {
     next(error);
