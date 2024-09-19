5ee54eff1b4a0475e61d1d845bba51ded74c05b1
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-09-19 17:14:08 +0300

    Commit for adding messages and contract routers

--- src/routers/contractRouter.mjs
+++ src/routers/contractRouter.mjs
@@ -1,8 +1,10 @@
 import express from 'express';
 import { verify, restrict } from '../controllers/authController.mjs';
-
+import { getAllContracts } from '../controllers/contractController.mjs';
 const router = express.Router();
 
-router.route(`/`).get();
+router
+  .route(`/`)
+  .get(verify, restrict([`client`, `freelancer`]), getAllContracts);
 
 export default router;
