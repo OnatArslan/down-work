7364a70c28f3e8db204ef770eff1effb4a8ea643
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-09-24 07:19:04 +0300

    Commit for changes

--- src/controllers/notificationController.mjs
+++ src/controllers/notificationController.mjs
@@ -2,8 +2,24 @@ import prisma from '../db/prisma.mjs';
 
 export const getNotifications = async (req, res, next) => {
   try {
+    const notifications = await prisma.notification.findMany({
+      where: {
+        receiverId: Number(req.user.id),
+      },
+      take: 20,
+      orderBy: {
+        createdAt: `desc`,
+      },
+      omit: {
+        id: true,
+        updatedAt: true,
+        receiverId: true,
+      },
+    });
+
     res.status(200).json({
       status: `success`,
+      notifications,
     });
   } catch (error) {
     next(error);
