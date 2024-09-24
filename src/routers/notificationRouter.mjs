0894722299f908701ba5c381c95ce6a66c19e7a9
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-09-24 07:15:22 +0300

    Commit for changes

--- src/routers/notificationRouter.mjs
+++ src/routers/notificationRouter.mjs
@@ -1,7 +1,11 @@
 import express from 'express';
 
+import { getNotifications } from '../controllers/notificationController.mjs';
+import { verify, restrict } from '../controllers/authController.mjs';
 const router = express.Router();
 
-router.route(`/`).get();
+router
+  .route(`/`)
+  .get(verify, restrict([`client`, `freelancer`]), getNotifications);
 
 export default router;
