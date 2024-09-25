9c1c2f73db38229b4353957db8a923e4ad31c27e
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-09-25 04:36:35 +0300

    Commit for changes

--- src/controllers/friendshipController.mjs
+++ src/controllers/friendshipController.mjs
@@ -16,3 +16,20 @@ export const getFollowers = async (req, res, next) => {
     next(error);
   }
 };
+
+export const sendFollowRequest = async (req, res, next) => {
+  try {
+    const followers = await prisma.follows.findMany({
+      where: {
+        followingId: Number(req.user.id),
+        status: `accepted`,
+      },
+    });
+    res.status(200).json({
+      status: `success`,
+      followers,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- src/routers/friendshipRouter.mjs
+++ src/routers/friendshipRouter.mjs
@@ -2,12 +2,19 @@ import express from 'express';
 
 import { verify, restrict } from '../controllers/authController.mjs';
 
-import { getFollowers } from '../controllers/friendshipController.mjs';
+import {
+  getFollowers,
+  sendFollowRequest,
+} from '../controllers/friendshipController.mjs';
 
 const router = express.Router();
 
 router
   .route(`/followers`)
   .get(verify, restrict([`client`, `freelancer`]), getFollowers);
+
+router
+  .route(`/:userId/follow`)
+  .post(verify, restrict([`client`, `freelancer`]), sendFollowRequest);
 
 export default router;
