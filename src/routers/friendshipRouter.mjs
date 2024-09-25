3343707e18c3160a311ba312ba77cc6667ae6a84
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-09-25 04:34:42 +0300

    Commit for friendship controller

--- src/routers/friendshipRouter.mjs
+++ src/routers/friendshipRouter.mjs
@@ -2,6 +2,12 @@ import express from 'express';
 
 import { verify, restrict } from '../controllers/authController.mjs';
 
+import { getFollowers } from '../controllers/friendshipController.mjs';
+
 const router = express.Router();
+
+router
+  .route(`/followers`)
+  .get(verify, restrict([`client`, `freelancer`]), getFollowers);
 
 export default router;
