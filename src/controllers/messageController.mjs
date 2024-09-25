8d75c2a071ddeba276e0ad26234240310cc1bae9
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-09-25 09:56:59 +0300

    Commit for message router and controller

--- src/controllers/messageController.mjs
+++ src/controllers/messageController.mjs
@@ -110,6 +110,7 @@ export const deleteMessages = async (req, res, next) => {
     }
     res.status(200).json({
       status: `success`,
+      message: `Messages deleted successfully`,
     });
   } catch (error) {
     next(error);
