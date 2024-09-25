c24ad021ed03ab02461ae1861d121abd892f8629
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-09-25 05:27:45 +0300

    Commit for changes

--- src/routers/friendshipRouter.mjs
+++ src/routers/friendshipRouter.mjs
@@ -9,6 +9,7 @@ import {
   acceptRequest,
   declineRequest,
   getFollowings,
+  removeFollower,
 } from '../controllers/friendshipController.mjs';
 
 const router = express.Router();
@@ -16,6 +17,9 @@ const router = express.Router();
 router.use(verify, restrict([`client`, `freelancer`]));
 
 router.route(`/followers`).get(getFollowers);
+
+router.route(`/followers/:followerId`).patch(removeFollower);
+
 router.route(`/followings`).get(getFollowings);
 
 router.route(`/:followingId/follow`).post(sendFollowRequest);
