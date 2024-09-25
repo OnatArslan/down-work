3f54afb28a99d3a66e14ee85e26ab2c51d709bf4
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-09-25 07:18:25 +0300

    Commit for message router and controller

---
+++ src/controllers/messageController.mjs
@@ -0,0 +1,66 @@
+import prisma from '../db/prisma.mjs';
+
+export const getMessages = async (req, res, next) => {
+  try {
+    const messages = await prisma.message.findMany({
+      where: {
+        OR: [
+          {
+            senderId: Number(req.user.id),
+            recieverId: Number(req.params.userId),
+          },
+          {
+            senderId: Number(req.params.userId),
+            recieverId: Number(req.user.id),
+          },
+        ],
+      },
+      omit: {
+        updatedAt: true,
+        senderId: true,
+        recieverId: true,
+      },
+      include: {
+        sender: {
+          select: { username: true },
+        },
+      },
+    });
+    res.status(200).json({
+      status: `success`,
+      messages,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const sendMessage = async (req, res, next) => {
+  try {
+    const messages = await prisma.message.findMany({
+      where: {
+        OR: [
+          {
+            senderId: Number(req.user.id),
+            recieverId: Number(req.params.userId),
+          },
+          {
+            senderId: Number(req.params.userId),
+            recieverId: Number(req.user.id),
+          },
+        ],
+      },
+      include: {
+        sender: {
+          select: { username: true },
+        },
+      },
+    });
+    res.status(200).json({
+      status: `success`,
+      messages,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- src/routers/messageRouter.mjs
+++ src/routers/messageRouter.mjs
@@ -1,8 +1,11 @@
 import express from 'express';
 import { verify, restrict } from '../controllers/authController.mjs';
+import { getMessages } from '../controllers/messageController.mjs';
 
 const router = express.Router();
 
 router.use(verify);
+
+router.route(`/:userId`).get(getMessages).post();
 
 export default router;
