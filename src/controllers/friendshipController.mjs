ca4443495ecb8ca9017dbc4fbd11d6ada6eb75ae
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-09-25 04:30:45 +0300

    Commit for changes

--- src/controllers/friendshipController.mjs
+++ src/controllers/friendshipController.mjs
@@ -1,1 +1,11 @@
 import prisma from '../db/prisma.mjs';
+
+export const getFollowers = async (req, res, next) => {
+  try {
+    res.status(200).json({
+      status: `success`,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
