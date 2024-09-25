c53cbd933716074e26c9e00e0e88ea710c3d4f74
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-09-25 04:33:21 +0300

    Commit for changes

--- src/controllers/friendshipController.mjs
+++ src/controllers/friendshipController.mjs
@@ -2,8 +2,15 @@ import prisma from '../db/prisma.mjs';
 
 export const getFollowers = async (req, res, next) => {
   try {
+    const followers = await prisma.follows.findMany({
+      where: {
+        followingId: Number(req.user.id),
+        status: `accepted`,
+      },
+    });
     res.status(200).json({
       status: `success`,
+      followers,
     });
   } catch (error) {
     next(error);
