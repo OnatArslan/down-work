60d769ebc13163d606fbb5c3010b309260cab5c9
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-09-29 10:44:41 +0300

    Commit for changes

--- src/app.mjs
+++ src/app.mjs
@@ -1,25 +1,26 @@
 // Importing required packages
-import express from 'express';
-import http from 'http';
-import cors from 'cors';
-import morgan from 'morgan';
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
-import helmet from 'helmet';
-import compression from 'compression';
-import expressSession from 'express-session';
-import { rateLimit } from 'express-rate-limit';
+import express from "express";
+import http from "http";
+import cors from "cors";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import compression from "compression";
+import expressSession from "express-session";
+import { rateLimit } from "express-rate-limit";
 
 // Import routers
-import userRouter from './routers/userRouter.mjs';
-import jobRouter from './routers/jobRouter.mjs';
-import authRouter from './routers/authRouter.mjs';
-import proposalRouter from './routers/proposalRouter.mjs';
-import profileRouter from './routers/profileRouter.mjs';
-import contractRouter from './routers/contractRouter.mjs';
-import messageRouter from './routers/messageRouter.mjs';
-import notificationRouter from './routers/notificationRouter.mjs';
-import friendshipRouter from './routers/friendshipRouter.mjs';
+import userRouter from "./routers/userRouter.mjs";
+import jobRouter from "./routers/jobRouter.mjs";
+import authRouter from "./routers/authRouter.mjs";
+import proposalRouter from "./routers/proposalRouter.mjs";
+import profileRouter from "./routers/profileRouter.mjs";
+import contractRouter from "./routers/contractRouter.mjs";
+import messageRouter from "./routers/messageRouter.mjs";
+import notificationRouter from "./routers/notificationRouter.mjs";
+import friendshipRouter from "./routers/friendshipRouter.mjs";
+
 // Create app
 const app = express();
 
@@ -28,20 +29,22 @@ const server = http.createServer(app);
 
 // Using npm package middlewares
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(helmet());
 app.use(compression());
+
 app.use(
   expressSession({
-    secret: 'your-secret-key',
+    secret: "your-secret-key",
     resave: false,
     saveUninitialized: true,
     cookie: { secure: true },
-  })
+  }),
 );
+
 // Rate limiting
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
@@ -62,11 +65,11 @@ app.use(`/friendship`, friendshipRouter);
 // Catch-all route for non-existent routes
 app.get(`*`, async (req, res, next) => {
   // const user = await prisma.user.create({});
-  res.status(404).json({ error: 'Route not found' });
+  res.status(404).json({ error: "Route not found" });
 });
 
 // Error-handling middleware
-app.use((err, req, res, next) => {
+app.use((err, res, next) => {
   console.error(err.message);
   res.status(500).json({ error: err.message });
 });
