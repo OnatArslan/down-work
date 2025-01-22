d387de838c83aa184d91e87f6c21d5dedb02df5e
OnatArslan <onatarslan@posta.mu.edu.tr>
2025-01-22 12:03:30 +0300

    Commit for changes

--- src/server.mjs
+++ src/server.mjs
@@ -3,7 +3,7 @@ import server from './app.mjs';
 
 const PORT = process.env.PORT || 3000;
 console.log(PORT);
-
+// This is neccessary for starting the server
 server.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
