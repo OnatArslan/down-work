f31701a066b942da7f663cd820f149816047b41f
Onat Arslan <142385726+OnatArslan@users.noreply.github.com>
2025-08-04 04:40:16 +0300

    Update app.mjs for delete bodyParser and use native express json and urlencoded parser

--- src/app.mjs
+++ src/app.mjs
@@ -3,7 +3,6 @@ import express from "express";
 import http from "http";
 import cors from "cors";
 import morgan from "morgan";
-import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import helmet from "helmet";
 import compression from "compression";
@@ -30,8 +29,8 @@ const server = http.createServer(app);
 // Using npm package middlewares
 app.use(cors());
 app.use(morgan("dev"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(helmet());
 app.use(compression());
